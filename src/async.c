b77e7c7d5fecce7d786234903fd11d3a37d5e303
aiverson <alexjiverson@gmail.com>
2018-07-19 06:04:11 -0600

    Allow sending uv_handle_t over an async.

--- src/async.c
+++ src/async.c
@@ -26,7 +26,7 @@ static uv_async_t* luv_check_async(lua_State* L, int index) {
 static void luv_async_cb(uv_async_t* handle) {
   lua_State* L = luv_state(handle->loop);
   luv_handle_t* data = (luv_handle_t*)handle->data;
-  int n = luv_thread_arg_push(L, (const luv_thread_arg_t*)data->extra, 0);
+  int n = luv_thread_arg_push(L, (const luv_thread_arg_t*)data->extra, LUVF_THREAD_UHANDLE);
   luv_call_callback(L, data, LUV_ASYNC, n);
   luv_thread_arg_clear(L, (luv_thread_arg_t*)data->extra, 0);
 }
@@ -55,7 +55,7 @@ static int luv_async_send(lua_State* L) {
   uv_async_t* handle = luv_check_async(L, 1);
   luv_thread_arg_t* arg = (luv_thread_arg_t *)((luv_handle_t*) handle->data)->extra;
   
-  luv_thread_arg_set(L, arg, 2, lua_gettop(L), 0);
+  luv_thread_arg_set(L, arg, 2, lua_gettop(L), LUVF_THREAD_UHANDLE);
   ret = uv_async_send(handle);
   if (ret < 0) return luv_error(L, ret);
   lua_pushinteger(L, ret);
